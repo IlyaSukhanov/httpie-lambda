c46d58669082818d2643ca38b496cff80e82d49e
Ilya Sukhanov <ilya@sukhanov.net>
2021-09-29 22:11:43 -0600

    Bump dependencies, namely httpie

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ from setuptools import setup
 
 setup(
     name="httpie-lambda",
-    version="0.1",
+    version="0.2",
     description="AWS Lambda invoke transport plugin for HTTPie",
     long_description=open("README.rst").read().strip(),
     author="Ilya Sukhanov",
     author_email="ilya@sukhanov.net",
     url="https://github.com/IlyaSukhanov/httpie-lambda",
     install_requires=[
-        "lambda-requests>=1.1",
-        "httpie>=2.4.0",
+        "lambda-requests>=1.4",
+        "httpie>=2.5.0",
     ],
     entry_points={
         "httpie.plugins.transport.v1": [
